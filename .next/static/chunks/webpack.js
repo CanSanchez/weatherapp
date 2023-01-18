79476ead307c0f87372382a9bd234d6417118231
CanSanchez <asanchez24@my.bcit.ca>
2023-01-18 11:00:33 -0800

    add button function

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c7429b6ff4343a5"; }
+/******/ 		__webpack_require__.h = function() { return "f85dfb47082e236f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
